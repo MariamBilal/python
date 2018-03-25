aed4db3509530bd01d965ae31118faf55e18c59e
Mariam Bilal <mariambilal6@gmail.com>
2018-03-25 18:15:44 +0500

    Practice

---
+++ User_input.py
@@ -0,0 +1,8 @@
+#To take input from user
+user_name = input("Enter your name : " )
+print(user_name)
+
+# Number input
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+print(num1+num2)
